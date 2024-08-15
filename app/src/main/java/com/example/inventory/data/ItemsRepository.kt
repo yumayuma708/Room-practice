21586c0ce260cff39211a3a5a7a4dd4546c35f43
yumayuma708 <yumaskywalker708@gmail.com>
2024-08-15 19:13:01 +0900

    Dao,Database,Repositoryを実装

--- app/src/main/java/com/example/inventory/data/ItemsRepository.kt
+++ app/src/main/java/com/example/inventory/data/ItemsRepository.kt
@@ -16,7 +16,20 @@
 
 package com.example.inventory.data
 
+import kotlinx.coroutines.flow.Flow
+
 /**
  * Repository that provides insert, update, delete, and retrieve of [Item] from a given data source.
  */
-interface ItemsRepository
+interface ItemsRepository{
+
+    fun getAllItemsStream(): Flow<List<Item>>
+
+    fun getItemStream(id: Int): Flow<Item?>
+
+    suspend fun insertItem(item: Item)
+
+    suspend fun updateItem(item: Item)
+
+    suspend fun deleteItem(item: Item)
+}
